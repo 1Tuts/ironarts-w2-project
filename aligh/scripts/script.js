af984798fdd392a48fdbfc943d86856adc0e5a7a
aligh <aghasizadeh70@gmail.com>
2012-06-17 00:22:45 +0430

    add rotate in toggle menu

--- aligh/scripts/script.js
+++ aligh/scripts/script.js
@@ -49,10 +49,41 @@ window.onload=function(){
 
 $(function(){
 		$('.hide-nav').click(function(){
-		$('.nav').stop().animate({'top':'-400px'},700,null,function(){
-			$('.hide-nav').click(function(){
+							$('.hide-nav').css(
+								'-webkit-transform','rotate(180deg)'
+							);
+							$('.hide-nav').css(
+								'-moz-transform','rotate(180deg)'
+							);
+							$('.hide-nav').css(
+								'-o-transform','rotate(180deg)'
+							);
+							$('.hide-nav').css(
+								'-ms-transform','rotate(180deg)'
+							);
+							$('.hide-nav').css(
+								'transform','rotate(180deg)'
+							);
+
+			$('.nav').stop().animate({'top':'-400px'},1000,null,function(){
+				$('.hide-nav').click(function(){
 					if($('.nav').css('top')=='-400px'){
-						$('.nav').stop().animate({'top':'0px'},700)
+							$('.nav').stop().animate({'top':'0px'},1000);
+							$('.hide-nav').css(
+								'-webkit-transform','rotate(0)'
+							);
+							$('.hide-nav').css(
+								'-moz-transform','rotate(0)'
+							);
+							$('.hide-nav').css(
+								'-o-transform','rotate(0)'
+							);
+							$('.hide-nav').css(
+								'-ms-transform','rotate(0)'
+							);
+							$('.hide-nav').css(
+								'transform','rotate(0)'
+							);
 					}
 			});
 			});
