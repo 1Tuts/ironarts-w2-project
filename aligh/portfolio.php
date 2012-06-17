0ff7546808e35697ba0add3afb08537dbef33c64
aligh <aghasizadeh70@gmail.com>
2012-06-17 22:01:46 +0430

    add and fix images (all save for web and fix width and height) pasha khoda marget bede ishalah X-(

--- aligh/portfolio.php
+++ aligh/portfolio.php
@@ -31,7 +31,7 @@
 			<div class="portfolio-content">
 				<h1>MY PORTFOLIO</h1>
 				<div class="sample w11 last">
-					<img class="left" src="images/doors/thumb.jpg"  width="251" height="211">
+					<img class="left" src="images/doors/thumb.jpg"  width="251" height="371">
 					<div class="desc">
 						<h1>consectetuer</h1>
 						<p>
@@ -44,23 +44,70 @@
 						<a href="images/doors/3.jpg" title="" rel="lightbox[group1]" ></a>
 						<a href="images/doors/4.jpg" title="" rel="lightbox[group1]" ></a>
 						<a href="images/doors/5.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/6.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/7.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/8.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/9.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/10.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/11.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/12.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/13.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/14.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/15.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/16.jpg" title="" rel="lightbox[group1]" ></a>
 					</div>
 				</div>
 				<div class="clear"></div>
 				<div class="sample w11 last">
-					<img class="left" src="images/chairs/thumb.jpg"  width="251" height="211">
+					<img class="left" src="images/staircase/thumb.jpg"  width="251" height="211">
 					<div class="desc">
 						<h1>consectetuer</h1>
 						<p>
 							Praesent vestibulum molestie lacus. Aenean nonummy 1999 hendrerit mauris. Phasellus porta. Fusce sucipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus 2001 mus. Nulla dui. Fusce feugiat malesuada odio. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. Maecenas 2011 tristique.
 						</p>
 						<div class="more">
-							<a href="images/chairs/1.jpg" title="" rel="lightbox[group2]">More Samples</a>
+							<a href="images/staircase/1.jpg" title="" rel="lightbox[group2]">More Samples</a>
 						</div>
-						<a href="images/chairs/2.jpg" title="" rel="lightbox[group2]" ></a>
-						<a href="images/chairs/3.jpg" title="" rel="lightbox[group2]" ></a>
-						<a href="images/chairs/4.jpg" title="" rel="lightbox[group2]" ></a>
-						<a href="images/chairs/5.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/2.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/3.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/4.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/5.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/6.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/7.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/8.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/9.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/10.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/11.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/12.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/13.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/14.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/15.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/16.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/16.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/17.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/18.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/19.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/20.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/21.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/22.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/23.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/24.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/25.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/26.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/27.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/28.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/29.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/30.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/31.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/32.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/33.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/34.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/35.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/36.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/37.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/38.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/39.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/40.jpg" title="" rel="lightbox[group2]" ></a>
 					</div>
 				</div>
 				<div class="clear"></div>
