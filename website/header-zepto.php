f9575d91efb50d890fb8cbfc6660aaf7a72eb606
aligh <aghasizadeh70@gmail.com>
2012-06-18 13:32:45 +0430

    add headers :)

---
+++ website/header-zepto.php
@@ -0,0 +1,10 @@
+	<script type="text/javascript" src="scripts/zepto.js"></script>
+	<script type="text/javascript" src="scripts/lightbox.js"></script>
+	<script type="text/javascript">
+		document.write('<script type="text/javascript" src=scripts/' + ('__proto__' in {} ? 'zepto' : 'jquery') + '.js><\/script>');
+	</script>
+	<script type="text/javascript" src="scripts/slideshow.js"></script>
+	<script type="text/javascript" src="scripts/script.js"></script>
+</head>
+<body>
+	<div class="header"></div>
---
+++ website/header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>:..LOUTOS IRON..:</title>
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/reset.css" />
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/1styles.css" />
+	<link rel="stylesheet" href="<?php bloginfo('template_url') ?>/lightbox.css" type="text/css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/mystyle.css" />
+	
+	<link href='http://fonts.googleapis.com/css?family=Julee|Fredericka+the+Great|Oleo+Script|Berkshire+Swash' rel='stylesheet' type='text/css'>
