5497eced9d043e16c3282fc6a12e3e260ffcb145
aligh <aghasizadeh70@gmail.com>
2012-06-18 14:14:20 +0430

    add some parts in wordpress

--- website/header-home.php
+++ website/header-home.php
@@ -8,3 +8,15 @@
 </head>
 <body>
 	<div class="header"></div>
+<div class="nav">
+	<div class="logo mla mra">
+		<h1>LOUTOS IRON</h1>
+	</div>
+	<ul class="mla mra">
+		<li class="home-li"><a href="index.php" style="background-color:black">HOME</a></li>
+		<li class="about-li"><a href="about.php">ABOUT</a></li>
+		<li class="port-li"><a href="portfolio.php">PORTFOLIO</a></li>
+		<li class="contanct-li"><a href="contact.php">CONTACTS</a></li>
+	</ul>
+	<div class="hide-nav mla mra"></div>
+</div>
--- website/index.php
+++ website/index.php
@@ -1,87 +1,14 @@
 <?php 
 
 	get_header();
-	get_template_part('header','zepto')
+	get_template_part('header','home')
 
  ?>
-
-	<div class="nav">
-		<div class="logo mla mra">
-			<h1>LOUTOS IRON</h1>
-		</div>
-		<ul class="mla mra">
-			<li class="home-li"><a href="index.php" style="background-color:black">HOME</a></li>
-			<li class="about-li"><a href="about.php">ABOUT</a></li>
-			<li class="port-li"><a href="portfolio.php">PORTFOLIO</a></li>
-			<li class="contanct-li"><a href="contact.php">CONTACTS</a></li>
-		</ul>
-		<div class="hide-nav mla mra"></div>	
-	</div>
-	<div class="container-content ml7 cover">		
-		</div>
-</div>
-<div class="clear"></div>
 	
 <!--footer-->
-	<div class="bottom-background" style="margin-top:600px;">
-		<div class="bottom-container">
-			<div class="bottom-content mla mra">
-				<div class="topic mla mra">
-				<div class="clear"></div>
-					<div class="art">
-						<h2>Articles</h2>
-						<ul>
-							<li><a href="#">Nothing for show</a></li>
-							<li><a href="#">Nothing for show </a></li>
-							<li><a href="#">Nothing for show</a></li>
-						</ul>
-					</div>
-					<div class="cat">
-						<h2>Categories</h2>
-						<ul>
-							<li><a href="#">Nothing for show</a></li>
-							<li><a href="#">Nothing for show</a></li>
-							<li><a href="#">Nothing for show</a></li>
-							<li><a href="#">Nothing for show</a></li>
-							<li><a href="#">Nothing for show</a></li>
-						</ul>
-					</div>
-					<div class="arc">
-						<h2>archive</h2>
-						<ul>
-							<li><a href="#">Nothing for show</a></li>
-							<li><a href="#">Nothing for show</a></li>
-							<li><a href="#">Nothing for show</a></li>
-							<li><a href="#">Nothing for show</a></li>
-							<li><a href="#">Nothing for show</a></li>
-						</ul>
-					</div>
-					<div class="fli cover">
-						<h2>flickr</h2>
-					</div>
-					<div class="clear"></div>
-				</div>
-			</div>
-		</div>
-		<div class="footer-container">
-			<div class="footer-content mla mra">
-				<div class="copy left">
-					<h4>Copyright © 2012. All rights reserved. check this project at <a href="https://github.com/1schools-projects/ironarts">Github</a></h4>
-				</div>
-				<div class="social">
-					<a href="http://www.facebook.com/hamid.rahbar.3"><img src="images/facebook.png" width="35" height="35" alt="facebook" /></a>
-					<a href="#"><img src="images/google-plus.jpg" width="32" height="32" alt="plus" /></a>
-					<a href="#"><img class="last-child" src="images/twitter.png" width="35" height="35" alt="twitter" /></a>
-				</div>
-			</div>
-		</div>
-	</div>
-<!--backImage-->
-	<div class="noise"></div>
-	<div class="backimg">
-			<script type="text/javascript">
-				for(var i=1;i<5;i++) document.write('<div style="background-image :url(images/'+i+'.jpg);"></div>');
-			</script>
-	</div>
-</body>
-</html>
+<?php
+	get_footer();
+	get_template_part('footer','backimage');
+	get_template_part('footer','close')
+?>
+
