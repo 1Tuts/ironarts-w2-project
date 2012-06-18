24b60f5c71f244a3cee8924e05bad215255f62bb
aligh <aghasizadeh70@gmail.com>
2012-06-18 23:49:00 +0430

    shift + delete due to GAND ZADAN :|

--- website/footer-backimage.php
+++
@@ -1,4 +0,0 @@
-<div class="backimg">
-<script type="text/javascript">
-	for(var i=1;i<5;i++) document.write('<div style="background-image :url(<?php bloginfo("template_url") ?>/images/'+i+'.jpg);"></div>');
-</script>
