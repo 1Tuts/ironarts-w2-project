754211ccf366d9df701875ee381be46a2b1b461a
ghasem <ghassemsadeghi@gmail.com>
2012-06-18 11:25:36 +0430

    add orginall to my folder

---
+++ ghasem/aligh asli/portfolio.php
@@ -0,0 +1,207 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>:..LOUTOS IRON..:</title>
+<link rel="stylesheet" type="text/css" href="reset.css" />
+<link rel="stylesheet" type="text/css" href="1styles.css" />
+<link rel="stylesheet" href="lightbox.css" type="text/css" media="screen" />
+<link rel="stylesheet" type="text/css" href="mystyle.css" />
+
+<link href='http://fonts.googleapis.com/css?family=Julee|Fredericka+the+Great|Oleo+Script|Berkshire+Swash' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="scripts/jquery.js"></script>
+<script type="text/javascript" src="scripts/lightbox.js"></script>
+<script type="text/javascript" src="scripts/script.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="nav left">
+			<div class="logo mla mra">
+				<h1>LOUTOS IRON</h1>
+			</div>
+			<ul class="mla mra">
+				<li class="home-li"><a href="index.php">HOME</a></li>
+				<li class="about-li"><a href="about.php">ABOUT</a></li>
+				<li class="port-li"><a href="portfolio.php" style="background-color:black">PORTFOLIO</a></li>
+				<li class="contanct-li"><a href="contact.php">CONTACTS</a></li>
+			</ul>
+		</div>
+		<div class="container-content ml7">
+<!------------portfolio------------>
+			<div class="portfolio-content">
+				<h1>MY PORTFOLIO</h1>
+				<div class="sample w11 last">
+					<img class="left" src="images/doors/thumb.jpg"   width="251" height="211">
+					<div class="desc">
+						<h1>DOORS</h1>
+						<p>
+							Praesent vestibulum molestie lacus. Aenean nonummy 1999 hendrerit mauris. Phasellus porta. Fusce sucipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus 2001 mus. Nulla dui. Fusce feugiat malesuada odio. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. Maecenas 2011 tristique.
+						</p>
+						<div class="more">
+							<a href="images/doors/1.jpg" title="" rel="lightbox[group1]">More Samples</a>
+						</div>
+						<a href="images/doors/2.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/3.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/4.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/5.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/6.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/7.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/8.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/9.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/10.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/11.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/12.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/13.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/14.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/15.jpg" title="" rel="lightbox[group1]" ></a>
+						<a href="images/doors/16.jpg" title="" rel="lightbox[group1]" ></a>
+					</div>
+				</div>
+				<div class="clear"></div>
+				<div class="sample w11 last">
+					<img class="left" src="images/staircase/thumb.jpg"  width="251" height="211">
+					<div class="desc">
+						<h1>STAIRCASES</h1>
+						<p>
+							Praesent vestibulum molestie lacus. Aenean nonummy 1999 hendrerit mauris. Phasellus porta. Fusce sucipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus 2001 mus. Nulla dui. Fusce feugiat malesuada odio. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. Maecenas 2011 tristique.
+						</p>
+						<div class="more">
+							<a href="images/staircase/1.jpg" title="" rel="lightbox[group2]">More Samples</a>
+						</div>
+						<a href="images/staircase/2.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/3.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/4.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/5.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/6.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/7.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/8.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/9.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/10.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/11.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/12.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/13.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/14.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/15.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/16.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/16.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/17.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/18.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/19.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/20.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/21.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/22.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/23.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/24.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/25.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/26.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/27.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/28.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/29.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/30.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/31.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/32.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/33.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/34.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/35.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/36.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/37.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/38.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/39.jpg" title="" rel="lightbox[group2]" ></a>
+						<a href="images/staircase/40.jpg" title="" rel="lightbox[group2]" ></a>
+					</div>
+				</div>
+				<div class="clear"></div>
+				<div class="sample w11 last">
+					<img class="left" src="images/furniture/thumb.jpg"  width="251" height="211">
+					<div class="desc">
+						<h1>FURNITURES</h1>
+						<p>
+							Praesent vestibulum molestie lacus. Aenean nonummy 1999 hendrerit mauris. Phasellus porta. Fusce sucipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus 2001 mus. Nulla dui. Fusce feugiat malesuada odio. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. Maecenas 2011 tristique.
+						</p>
+						<div class="more">
+							<a href="images/furniture/1.jpg" title="" rel="lightbox[group3]">More Samples</a>
+						</div>
+						<a href="images/furniture/2.jpg" title="" rel="lightbox[group3]" ></a>
+						<a href="images/furniture/3.jpg" title="" rel="lightbox[group3]" ></a>
+						<a href="images/furniture/4.jpg" title="" rel="lightbox[group3]" ></a>
+						<a href="images/furniture/5.jpg" title="" rel="lightbox[group3]" ></a>
+						<a href="images/furniture/6.jpg" title="" rel="lightbox[group3]" ></a>
+						<a href="images/furniture/7.jpg" title="" rel="lightbox[group3]" ></a>
+						<a href="images/furniture/8.jpg" title="" rel="lightbox[group3]" ></a>
+						<a href="images/furniture/9.jpg" title="" rel="lightbox[group3]" ></a>
+						<a href="images/furniture/10.jpg" title="" rel="lightbox[group3]" ></a>
+						<a href="images/furniture/11.jpg" title="" rel="lightbox[group3]" ></a>
+						<a href="images/furniture/12.jpg" title="" rel="lightbox[group3]" ></a>
+						<a href="images/furniture/13.jpg" title="" rel="lightbox[group3]" ></a>
+						<a href="images/furniture/14.jpg" title="" rel="lightbox[group3]" ></a>
+						<a href="images/furniture/15.jpg" title="" rel="lightbox[group3]" ></a>
+						<a href="images/furniture/16.jpg" title="" rel="lightbox[group3]" ></a>
+						<a href="images/furniture/17.jpg" title="" rel="lightbox[group3]" ></a>
+						<a href="images/furniture/18.jpg" title="" rel="lightbox[group3]" ></a>
+					</div>
+				</div>
+				<div class="clear"></div>
+		</div>
+	</div>
+	</div>
+<!------------footer------------>
+	<div class="bottom-background">
+		<div class="bottom-container">
+			<div class="bottom-content mla mra">
+				<div class="topic mla mra">
+				<div class="clear"></div>
+					<div class="art">
+						<h2>Articles</h2>
+						<ul>
+							<li><a href="#">test testtest test testtest </a></li>
+							<li><a href="#">test testtest test testtest </a></li>
+							<li><a href="#">test testtest test testtest </a></li>
+						</ul>
+					</div>
+					<div class="cat">
+						<h2>Categories</h2>
+						<ul>
+							<li><a href="#">test testtest test testtest </a></li>
+							<li><a href="#">test testtest test testtest </a></li>
+							<li><a href="#">test testtest test testtest </a></li>
+							<li><a href="#">test testtest test testtest </a></li>
+							<li><a href="#">test testtest test testtest </a></li>
+						</ul>
+					</div>
+					<div class="arc">
+						<h2>archive</h2>
+						<ul>
+							<li><a href="#">test testtest testtest test testtest </a></li>
+							<li><a href="#">test testtest test test test testtest </a></li>
+							<li><a href="#">test testtest testtest test  testtest </a></li>
+							<li><a href="#">test testtest testtest  testtest </a></li>
+							<li><a href="#">test testtest test testtest  test </a></li>
+						</ul>
+					</div>
+					<div class="fli cover">
+						<h2>flickr</h2>
+					</div>
+					<div class="clear"></div>
+				</div>
+			</div>
+		</div>
+		<div class="footer-container">
+			<div class="footer-content mla mra">
+				<div class="copy left">
+					<h4>Copyright © 2012. All rights reserved. check this project at <a href="https://github.com/1schools-projects/ironarts">Github</a></h4>
+				</div>
+				<div class="social">
+					<a href="http://www.facebook.com/hamid.rahbar.3"><img src="images/facebook.png" width="35" height="35" alt="facebook" /></a>
+					<a href="#"><img src="images/google-plus.jpg" width="32" height="32" alt="plus" /></a>
+					<a href="#"><img class="last-child" src="images/twitter.png" width="35" height="35" alt="twitter" /></a>
+				</div>
+			</div>
+		</div>
+	</div>
+<!------------backImage------------>
+	<div class="backimg">
+		<div class="img1" style="opacity:.7"></div>
+		<div style="background-color:black;opacity:.7;"></div>
+	</div>
+</body>
+</html>
