108f5d023a31bd57762f9dbd93c9411c142abf63
ghasem <ghassemsadeghi@gmail.com>
2012-06-21 11:31:50 +0430

    taghoorate folder to start wp

---
+++ ghasem/ironArts wp/index.php
@@ -0,0 +1,25 @@
+<?php include('header.php') ?>
+	<div class="nav nav-index left">
+		<?php include('main-menu.php') ?>
+		<div class="hide-nav mla mra"></div>
+	</div>
+
+	<div class="container-content ml7 cover"> </div>
+<div class="clear"></div>
+
+
+<!--..........footer.........-->
+<div class="bottom-background" style='margin-top:650px;'>
+	<?php include('footer.php') ?>
+</div>
+<!--.........backImage.........-->
+
+<div class="noise"></div>
+<div class="backimg slideshow">
+	<script type="text/javascript">
+		for(var i=1;i<5;i++) document.write('<div style="background-image :url(images/'+i+'.jpg);"></div>');
+	</script>
+</div>
+
+</body>
+</html>
