fe22efde0a74b21f6734af6f40f8835224d6e04b
ghasem <ghassemsadeghi@gmail.com>
2012-06-21 16:55:56 +0430

    meghdari taghiirat in wp

--- ghasem/ironArts wp/index.php
+++ ghasem/ironArts wp/index.php
@@ -1,12 +1,33 @@
 <?php get_header(); ?>
+<div class="container">
 	<div class="nav nav-index left">
 		<?php 	get_template_part('main', 'menu'); ?>
 		<div class="hide-nav mla mra"></div>
 	</div>
 
-	<div class="container-content ml7 cover"> </div>
-<div class="clear"></div>
+	<div class="container-content ml7 cover"> 
 
+	<?php 
+/*  in ja bayad be jaie post name   page ha gozashte beshe   man balad nabodam*/
+	if($post->post_name == 'Portfolio'){
+		get_template_part('portfolio','content');
+	}
+	else if($post->post_name == 'About us'){
+		get_template_part('about','content');
+	}
+	else if($post->post_name == 'contact us'){
+		get_template_part('contact','content');
+	}
+	else{
+		echo 'Nothing................................';
+	}
+/*  in ja bayad be jaie post name   page ha gozashte beshe   man balad nabodam*/
+	 ?>
+
+	</div>
+	<div class="clear"></div>
+
+</div>
 
 <!--..........footer.........-->
 <div class="bottom-background" style='margin-top:650px;'>
